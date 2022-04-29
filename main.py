f175e34a49d7cb2591ea47da09b9bd55bf49a0d2
Skidle <vovivave@gmail.com>
2022-04-29 17:44:25 +0300

    Добавил два режима

--- main.py
+++ main.py
@@ -1,40 +1,76 @@
+import os
+import time as tiime
+
 import discord
 from random import shuffle
 
-from requests import get
+import requests
 
 from key import TOKEN
 from data.db_session import global_init, create_session
 from data.user import User
 from data.country import Country
-from random import randint, randrange
+from random import randrange
 
 giving_roles = [(10, 'Новичок в географии'), (50, 'Начинающий географ'), (100, 'Любитель географии'),
                 (200, 'Прошаренный географ'), (500, 'Знаток географии'), (1000, 'Настоящий мастер в географии'),
                 (1500, 'Гуру-географ')]
 
 
-def score_count(user, message):
+async def score_count(user, message):
     global db_sess
     score = user.score
     roles = message.author.guild.roles
     for elem in giving_roles:
         if score == elem[0]:
             if elem[1] not in [role.name for role in roles]:
-                message.author.guild.create_role(elem[1], color=discord.Color.from_rgb(randrange(256), randrange(256),
-                                                                                       randrange(256)))
+                await message.author.guild.create_role(name=elem[1],
+                                                       color=discord.Color.from_rgb(randrange(256), randrange(256),
+                                                                                    randrange(256)))
             roles = message.author.guild.roles
-            role_id = [role.id for role in roles if role.name == elem[1]]
-            message.author.give_roles(discord.Object(role_id))
+            role_id = [role.id for role in roles if role.name == elem[1]][0]
+            await message.author.add_roles(discord.Object(role_id))
             if elem[0] != 10:
-                role_for_delete = [elem_ for elem_ in message.author.guild.roles if elem_.name == giving_roles[giving_roles.index(elem) - 1][1]][0]
-                message.author.remove_roles(role_for_delete)
+                role_for_delete = [elem_ for elem_ in message.author.guild.roles if
+                                   elem_.name == giving_roles[giving_roles.index(elem) - 1][1]][0]
+                await message.author.remove_roles(role_for_delete)
             if elem[0] == 100:
                 user.difficult = 2
                 db_sess.commit()
             elif elem[0] == 500:
                 user.difficult = 3
                 db_sess.commit()
+
+
+# def picture_of_object(toponym_to_find):
+#     # Возвращает контент картинки, который надо записать в файл
+#     geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
+#     geocoder_params = {
+#         "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
+#         "geocode": toponym_to_find,
+#         "format": "json"}
+#     response = requests.get(geocoder_api_server, params=geocoder_params)
+#     json_response = response.json()
+#     toponym = json_response["response"]["GeoObjectCollection"][
+#         "featureMember"][0]["GeoObject"]
+#
+#     toponym_coodrinates = toponym["Point"]["pos"]
+#     toponym_longitude, toponym_lattitude = toponym_coodrinates.split(" ")
+#
+#     lower_corner = toponym['boundedBy']['Envelope']['lowerCorner'].split()
+#     upper_corner = toponym['boundedBy']['Envelope']['upperCorner'].split()
+#     delta1 = abs(float(upper_corner[0]) - float(lower_corner[0]))
+#     delta2 = abs(float(upper_corner[1]) - float(upper_corner[1]))
+#     map_params = {
+#         'll': ",".join([toponym_longitude, toponym_lattitude]),
+#         'spn': ",".join([str(25), str(25)]),
+#         'l': 'sat,skl',
+#         "pt": f"{','.join([toponym_longitude, toponym_lattitude])},pm2dgl"
+#     }
+#     print(map_params)
+#     map_api_server = 'http://static-maps.yandex.ru/1.x/'
+#     response = requests.get(map_api_server, params=map_params)
+#     return response
 
 
 class GeoGesser(discord.Client):
@@ -59,10 +95,10 @@ class GeoGesser(discord.Client):
                 except IndexError:
                     country = db_sess.query(Country).filter(Country.id == country_id)
                     pairs = country.name, country.capital
-                    if message.content.lower() == pairs[0][1].lower():
+                    if message.content.lower() == pairs[1].lower():
                         user.score += 1
                         db_sess.commit()
-                        score_count(user, message)
+                        await score_count(user, message)
                         await message.channel.send('Right!')
                         await message.channel.send(f'You win the game! Your general score is {user.score}')
                         user.processing = None
@@ -81,7 +117,7 @@ class GeoGesser(discord.Client):
                 if message.content.lower() == pairs[0][1].lower():
                     user.score += 1
                     db_sess.commit()
-                    score_count(user, message)
+                    await score_count(user, message)
                     await message.channel.send('Right!')
                     await message.channel.send(pairs[1][0])
                     mlist = ' '.join(mlist[1:])
@@ -102,10 +138,10 @@ class GeoGesser(discord.Client):
                 except IndexError:
                     country = db_sess.query(Country).filter(Country.id == country_id)
                     pairs = country.name, country.capital
-                    if message.content.lower() == pairs[0][1].lower():
+                    if message.content.lower() == pairs[0].lower():
                         user.score += 1
                         db_sess.commit()
-                        score_count(user, message)
+                        await score_count(user, message)
                         await message.channel.send('Right!')
                         await message.channel.send(f'You win the game! Your general score is {user.score}')
                         user.processing = None
@@ -124,7 +160,7 @@ class GeoGesser(discord.Client):
                 if message.content.lower() == pairs[0][0].lower():
                     user.score += 1
                     db_sess.commit()
-                    score_count(user, message)
+                    await score_count(user, message)
                     await message.channel.send('Right!')
                     await message.channel.send(pairs[1][1])
                     mlist = ' '.join(mlist[1:])
@@ -136,6 +172,71 @@ class GeoGesser(discord.Client):
                     db_sess.commit()
                     await message.channel.send('Wrong')
                     await message.channel.send('The game have finished')
+            elif str(processing) == '3':
+                mlist = user.ids_of_countries
+                mlist = mlist.split()
+                country_id = mlist[0]
+                try:
+                    next_country_id = mlist[1]
+                except IndexError:
+                    country = db_sess.query(Country).filter(Country.id == country_id)
+                    pairs = country.name, country.flag
+                    if message.content.lower() == pairs[0].lower():
+                        user.score += 1
+                        db_sess.commit()
+                        await score_count(user, message)
+                        await message.channel.send('Right!')
+                        await message.channel.send(f'You win the game! Your general score is {user.score}')
+                        user.processing = None
+                        user.ids_of_countries = None
+                        db_sess.commit()
+                    else:
+                        user.processing = None
+                        user.ids_of_countries = None
+                        db_sess.commit()
+                        await message.channel.send('Wrong')
+                        await message.channel.send('The game have finished')
+                country_1 = db_sess.query(Country).filter(Country.id == country_id).first()
+                country_2 = db_sess.query(Country).filter(Country.id == next_country_id).first()
+                pairs = ((country_1.name, country_1.flag), (country_2.name, country_2.flag))
+                if message.content.lower() == pairs[0][0].lower():
+                    user.score += 1
+                    db_sess.commit()
+                    await score_count(user, message)
+                    await message.channel.send('Right!')
+                    await message.channel.send(pairs[1][1])
+                    mlist = ' '.join(mlist[1:])
+                    user.ids_of_countries = mlist
+                    db_sess.commit()
+                else:
+                    user.processing = None
+                    user.ids_of_countries = None
+                    db_sess.commit()
+                    await message.channel.send('Wrong')
+                    await message.channel.send('The game have finished')
+            elif str(processing) == '4':
+                mlist = user.ids_of_countries
+                mlist = mlist.split()
+                if message.content.lower() in mlist:
+                    mlist.remove(message.content.lower())
+                    user.score += 1
+                    await score_count(user, message)
+                    db_sess.commit()
+                    user.ids_of_countries = mlist
+                    if len(mlist) == 1:
+                        await message.channel.send('You named all of countries')
+                        count = user.score - int(mlist[-1][:-7])
+                        time = tiime.time() - user.nick
+                        await message.channel.send(f'Your score is {count} in {round(time)} seconds')
+
+                else:
+                    await message.channel.send('Oops! You named a wrong country. Game have finished')
+                    count = user.score - int(mlist[-1][:-7])
+                    time = tiime.time() - user.nick
+                    await message.channel.send(f'You wrote {count} countries in {round(time)} seconds')
+                    user.processing = None
+                    user.ids_of_countries = None
+                    db_sess.commit()
         else:
             if message.content == '!!game 1':
                 name = message.author.name
@@ -147,6 +248,7 @@ class GeoGesser(discord.Client):
                     user.name = name
                     user.difficult = 1
                     user.score = 0
+                    db_sess.add(user)
                     db_sess.commit()
                     id = user.id
                 user.processing = 1
@@ -169,6 +271,7 @@ class GeoGesser(discord.Client):
                     user.name = name
                     user.difficult = 1
                     user.score = 0
+                    db_sess.add(user)
                     db_sess.commit()
                     id = user.id
                 user.processing = 2
@@ -182,7 +285,49 @@ class GeoGesser(discord.Client):
                 user.ids_of_countries = mlist
                 db_sess.commit()
             elif message.content == '!!game 3':
-                pass
+                name = message.author.name
+                try:
+                    user = db_sess.query(User).filter(User.name == name).first()
+                    id = user.id
+                except Exception:
+                    user = User()
+                    user.name = name
+                    user.difficult = 1
+                    user.score = 0
+                    db_sess.add(user)
+                    db_sess.commit()
+                    id = user.id
+                user.processing = 3
+                countries = db_sess.query(Country).filter(Country.difficult <= user.difficult).all()
+                mlist = [str(elem.id) for elem in countries]
+                shuffle(mlist)
+                country_id = mlist[0]
+                mlist = ' '.join(mlist)
+                country = db_sess.query(Country).filter(Country.id == country_id).first()
+                await message.channel.send(country.flag)
+                user.ids_of_countries = mlist
+                db_sess.commit()
+            elif message.content == '!!game 4':
+                name = message.author.name
+                try:
+                    user = db_sess.query(User).filter(User.name == name).first()
+                    id = user.id
+                except Exception:
+                    user = User()
+                    user.name = name
+                    user.difficult = 1
+                    user.score = 0
+                    db_sess.add(user)
+                    db_sess.commit()
+                    id = user.id
+                user.processing = 4
+                user.nick = tiime.time()
+                countries = db_sess.query(Country).all()
+                mlist = [elem.name.lower() for elem in countries]
+                mlist.append(f'{user.score}.......')
+                mlist = ' '.join(mlist)
+                user.ids_of_countries = mlist
+                db_sess.commit()
             elif message.content == '!!stop':
                 name = message.author.name
                 try:
